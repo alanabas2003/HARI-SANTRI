a1e6cc21372c6c7ad1a3a8fed35ee4f5fb01ccc0
alanabas2003 <alanabbasumar0@gmail.com>
2024-10-22 10:45:58 +0700

    Add files via upload

---
+++ info.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar php</title>
+</head>
+<body>
+    <?php
+    echo"<h1>Helow word!<h1/>"
+    ?>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut reiciendis cupiditate ratione perspiciatis aliquam veniam voluptas, saepe dolor voluptate nobis repellendus ipsa excepturi esse reprehenderit fugiat repudiandae dicta ut. Corrupti!</p>
+</body>
+</html>
